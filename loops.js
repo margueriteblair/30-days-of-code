b0c03106d4b7edf20c587139f1103c57a9aa00ea
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-12 12:32:28 -0400

    loops and template literals to display equations

---
+++ loops.js
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    let result = 0;
+    for (let i = 1; i <=10; i++) {
+        result = n*i;
+        console.log(`${n} x ${i} = ${result}`);
+    }
+
+}
