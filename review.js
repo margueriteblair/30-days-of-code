90999ef9c1d7b5446e0e053ccf4f380fe3e68eca
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-13 11:52:28 -0400

    second function to join the two together

--- review.js
+++ review.js
@@ -14,3 +14,21 @@ function processData(str) {
 } 
 
 processData('Hello World');
+
+function processData2(input) {
+    let S = input.split(/\n/).slice(1)
+    for (let i = 0; i < S.length; i++) {
+        let evens = [];
+        let odds = [];
+        for (let j = 0; j < S[i].length; j++) {
+        if (j % 2 === 0) {
+            evens.push(S[i][j])
+        } else if (j % 2 === 1) {
+            odds.push(S[i][j])
+        }
+        }
+    evens = evens.join("");
+    odds = odds.join("");
+    console.log(evens, odds);
+    }
+} 
