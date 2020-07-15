9fabc688f677061bd55d99ef0be6367906ad4885
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-15 10:28:31 -0400

    object to make a phone book

---
+++ map.js
@@ -0,0 +1,21 @@
+function processData(input) {
+    let arr = input.slice(1).split(/\n/);
+    let phoneBook = {
+        this.name=name,
+        this.number=number
+    }
+    for (i = 1; i < 4; i++) {
+        
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
