5d68f507cff3eaa6ac9302312d655107984d5d52
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-16 13:58:40 -0400

    factorial using recursion

--- map.js
+++ map.js
@@ -1,21 +1,34 @@
 function processData(input) {
-    let arr = input.slice(1).split(/\n/);
-    let phoneBook = {
-        this.name=name,
-        this.number=number
-    }
-    for (i = 1; i < 4; i++) {
-        
-    }
-} 
-
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
-});
-
-process.stdin.on("end", function () {
-   processData(_input);
-});
+    const arr = input.split(/\n/); 
+    const map = new Map(); 
+    const n = +arr[0];
+   for(let i = 1; i < arr.length; i++) {
+       const [key, value] = arr[i].split(' ');
+       if(value !== undefined) {
+           map.set(key, +value);
+       } else {
+           const searchedString = arr[i];
+           console.log(validator(searchedString, map));
+       }
+   }
+   }
+   function validator(key, map) { 
+       if(map.has(key)) {
+       const value = map.get(key); 
+           return key+'='+value
+        } return 'Not found'; 
+        };
+   
+   process.stdin.on("end", function () { processData(_input); });
+   
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   _input = "";
+   process.stdin.on("data", function (input) {
+       _input += input;
+   });
+   
+   process.stdin.on("end", function () {
+      processData(_input);
+   });
+   
